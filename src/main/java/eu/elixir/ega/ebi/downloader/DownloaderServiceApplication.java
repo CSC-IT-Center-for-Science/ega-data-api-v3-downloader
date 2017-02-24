e3c4c5196ddd9b89e2d3db49cac2b1826dbc8f1c
asenf <asenf@asenf-virtual-machine>
2017-02-24 10:22:43 +0000

    Added Swagger

--- src/main/java/eu/elixir/ega/ebi/downloader/DownloaderServiceApplication.java
+++ src/main/java/eu/elixir/ega/ebi/downloader/DownloaderServiceApplication.java
@@ -20,8 +20,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableDiscoveryClient
 public class DownloaderServiceApplication implements CommandLineRunner {
 
@@ -38,5 +45,15 @@ public class DownloaderServiceApplication implements CommandLineRunner {
                 System.out.println("Test Code");
                 
                 
-	}
+	}        
+        
+        @Bean
+        public Docket swaggerSettings() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build()
+                    .pathMapping("/");
+        }
 }
